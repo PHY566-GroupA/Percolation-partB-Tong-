647360592851a6080ee8ed583783c302d18de91e
ericzhut <ericzhut@gmail.com>
2015-04-13 22:02:52 -0400

    Add comments and all the plot and document

--- version2.py
+++ version2.py
@@ -9,8 +9,8 @@ from pylab import *
 import matplotlib as mpl
 import random
 import time
-
-
+from scipy import optimize
+import matplotlib.colors
 
 
 def initialize(n):
@@ -74,6 +74,10 @@ def percolation(num1,num2,xcluster,ycluster,grid):
 	return
 
 def check(xcluster,ycluster,n):
+    '''
+    check if there exist span cluster , return the span cluster tuple and the ans value
+    when there exist span cluster ans=0, when there exist no span cluster ans=1
+    '''
     ans = 1
     span = []
     for i in range(len(xcluster)):
@@ -89,6 +93,9 @@ def check(xcluster,ycluster,n):
     return ans, span
 
 def combine(x,y,n,xcluster,ycluster,grid):
+    '''
+    combination of clusters when they are juncked,
+    '''
     if x<n-1:
         if (grid[x+1][y]):
             num1=int(grid[x+1][y])
@@ -114,6 +121,10 @@ def combine(x,y,n,xcluster,ycluster,grid):
 
 
 def solution(n,p):
+    '''
+    one step of calculation, return the fraction number, the span clusters and all clusters
+    if F=0, there are no span cluster existed
+    '''
     grid,vacancy = initialize(n)
     ans=1
     clnum=1
@@ -143,6 +154,9 @@ def solution(n,p):
 
 
 def final(n,p):
+    '''
+    for a sequence value of p, doing a series calculation of 50 times, return the effective average number of F.
+    '''
     l=len(p)
     total=zeros((l,50))
     F=zeros(l)
@@ -157,34 +171,86 @@ def final(n,p):
         print a
         a=0
     return F,a
+ 
 
-p=linspace(0.60,0.70,5)
-F,a=final(50,p)
+'''   
+#show the one configuration of cluster 
+F,span,xcluster,ycluster,grid=solution(100,0.59)
+l=len(xcluster)
+cl=np.linspace(0,1,l)
+figure(figsize=(10,10))
+for i in range(len(xcluster)):
+    if (len(xcluster[i])!=0):
+        plot(xcluster[i],ycluster[i],'o')
+savefig('show0.59-2.png',dpi=800)
+
+'''    
+    
+'''
+p=linspace(0.60,0.90,30)
+F,a=final(100,p)
 print p
 print F
 plt.plot(p,F)
 '''
-p=array([ 0.65 ,       0.65689655 , 0.6637931   ,0.67068966 , 0.67758621,  0.68448276,
-  0.69137931 , 0.69827586 , 0.70517241 , 0.71206897 , 0.71896552 , 0.72586207,
-  0.73275862 , 0.73965517 , 0.74655172 , 0.75344828 , 0.76034483 , 0.76724138 ,
-  0.77413793 , 0.78103448 , 0.78793103 , 0.79482759 , 0.80172414 , 0.80862069,
-  0.81551724 , 0.82241379 , 0.82931034 , 0.8362069  , 0.84310345 , 0.85      ]
-)
-F=array([ 0.35937038 , 0.38004543 , 0.44000397 , 0.4115973 ,  0.39243808 , 0.36536844,
-  0.44173034 , 0.37792407 , 0.39071663 , 0.3970174  , 0.41946921 , 0.38999831,
-  0.42840531 , 0.40828894 , 0.41540073 , 0.39130996 , 0.39255681 , 0.39857331,
-  0.41476819 , 0.42124591 , 0.39507906 , 0.38222086 , 0.41098439 , 0.40689961,
-  0.39580929 , 0.41692609 , 0.42680671 , 0.409812 ,   0.42114223 , 0.42629931]
-)       
 
+'''
+# results of solution of 100 grids. 
+p=array([0.6 , 0.61034483 , 0.62068966 , 0.63103448 , 0.64137931 , 0.65172414,
+  0.66206897 , 0.67241379 , 0.68275862 , 0.69310345 , 0.70344828 , 0.7137931,
+  0.72413793 , 0.73448276 , 0.74482759 , 0.75517241 , 0.76551724 , 0.77586207,
+  0.7862069  , 0.79655172 , 0.80689655 , 0.81724138 , 0.82758621 , 0.83793103,
+  0.84827586 , 0.85862069 , 0.86896552 , 0.87931034 , 0.88965517 , 0.9       ]
+
+)
+F=array([0.56404391 , 0.69646764 , 0.79843208 , 0.86442536 , 0.90419613 , 0.9200564,
+  0.94378756 , 0.95536067 , 0.96459492 , 0.97172341 , 0.97645554 , 0.98123457,
+  0.98592279 , 0.98822664 , 0.99041563 , 0.99276496 , 0.99380738 , 0.99509342,
+  0.9960971  , 0.99696807 , 0.9974838  , 0.9982    , 0.9985756  , 0.99882591,
+  0.99912255 , 0.99942017 , 0.99947953 , 0.9996425 ,  0.99975722 , 0.99984228]
+
+) 
+
+plt.xlabel("$Occupation\  Probability \ p$",fontsize=16)
+plt.ylabel("$Fraction \ F$", fontsize=16)
+plt.plot(p,F,'r')
+plt.savefig('P1.png',dpi=800)    
+
+'''
+
+
+'''
+#fit solution of problem b)
+p=array([0.6 , 0.61034483 , 0.62068966 , 0.63103448 , 0.64137931 , 0.65172414])
+F=array([0.56404391 , 0.69646764 , 0.79843208 , 0.86442536 , 0.90419613 , 0.9200564])
 l=len(p)
 plog=zeros(l)
 Flog=zeros(l)
+Ffit=zeros(l)
 for i in range(l):
-    plog[i]=log(p[i]-0.62)
+    plog[i]=log(p[i])
     Flog[i]=log(F[i])
-plt.plot(plog,Flog)    
+    
+
+def fit(x,a,b,c):
+    return a+b*log(x-c)
+
+guess=array([0.8,0.6,0.4])
+a,b=optimize.curve_fit(fit,p,Flog,guess)
+print a
+for i in range(l):
+    Ffit[i]=fit(p[i],a[0],a[1],a[2])
+print Ffit
+toWrite= "fit curve: ln(F) = %f + %f*ln(p-%f)" % (a[0],a[1],a[2])
+plt.xlabel("$Occupation\  Probability \ p$",fontsize=16)
+plt.ylabel("$Fraction \ ln(F)$", fontsize=16)
+plt.plot(p,Ffit,'r',label="$Curve \ fit$")
+plt.plot(p,Flog,'b',label="$Calculation \ Result$")
+plt.text(0.61,-0.4,toWrite)
+plt.legend(loc='best',fontsize=14)
+plt.savefig('P2.png',dpi=800)
 '''
+#plt.plot(p,Ffit,'b')
 #F,span,xcluster,ycluster,grid = solution(5,0.6)
 #print F
 #print span
